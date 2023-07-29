478eddd173c782b825563fdd021655cc600a0b0a
Aslam Shaikh <beingaslam@outlook.in>
2023-07-29 20:53:38 +0530

    Add project files.

---
+++ ExcelApplication.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExcelApplication.WebUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
